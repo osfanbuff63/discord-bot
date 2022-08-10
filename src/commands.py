94fd06dd7161c2eff96c104fbee50a683f3d4620
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2022-08-10 09:07:02 -0400

    uh changes i guess

--- src/commands.py
+++ src/commands.py
@@ -1,16 +1,14 @@
-from turtle import hideturtle
 from discord.ext import commands
 import discord
 from random import choice
 
-bot = commands.Bot(command_prefix='!', helpCommand=help)
-client = discord.client
+bot = commands.Bot(command_prefix="!", helpCommand=help)
 
-@bot.command
+@bot.command(name="github")
 async def github(ctx):
-    await ctx.send('Check out osfanbuff63\'s projects on GitHub here! https://github.com/osfanbuff63')
+    await ctx.send("Check out osfanbuff63\"s projects on GitHub here! https://github.com/osfanbuff63")
 
-@bot.command
+@bot.command(name="rules")
 async def rules(ctx):
     await ctx.send(
      "*This is copied from #info. Current as of August 7th, 2022*"
@@ -23,21 +21,21 @@ async def rules(ctx):
 
 #@info.command()
 #async def source(ctx):
-#    await ctx.send("Check out and contribute to the bot's source code here: https://github.com/osfanbuff63/discord-bot")
+#    await ctx.send("Check out and contribute to the bot"s source code here: https://github.com/osfanbuff63/discord-bot")
 
 #@info.command()
 #async def discord_bot(ctx):
 #    await ctx.send(
-#        "osfanbuff63\'s Discord Bot version {version}"
+#        "osfanbuff63\"s Discord Bot version {version}"
 #        "Licensed under the MIT License"
 #        "For the source code, run `!info source`"
 #    )
 
 @bot.command
 async def hug(ctx, arg):
-    await ctx.send(f'{ctx.message.author.mention} hugs {arg}')
+    await ctx.send(f"{ctx.message.author.mention} hugs {arg}")
 
 @bot.command
 async def user(ctx):
     user = choice(ctx.message.channel.guild.members)
-    await ctx.send(f'Today\'s random user is {user}')
+    await ctx.send(f"Today\"s random user is {user}")
--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ import commands
 import logger
 import discord
 
-version = 'v0.1.0-SNAPSHOT'
+version = "v0.1.0-SNAPSHOT"
 client = discord.Client()
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv("DISCORD_TOKEN")
 running_on = os.name
-license = 'MIT License'
-repo = 'https://github.com/osfanbuff63/discord-bot'
+license = "MIT License"
+repo = "https://github.com/osfanbuff63/discord-bot"
 
-print(f"osfanbuff63\'s Discord Bot version {version} running on {running_on}")
+print(f"osfanbuff63\"s Discord Bot version {version} running on {running_on}")
 print(f"Licensed under the {license}")    
 print(f"Source code available here: {repo}")
 print("")
@@ -18,6 +18,6 @@ print("Starting up!")
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
 
 client.run(token)
