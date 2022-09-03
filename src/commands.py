e9168357b4349f3b3ee9962aef00171f32fd20db
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2022-09-03 08:05:49 -0400

    Refactor to Docker and discord.py 2.0

--- src/commands.py
+++ src/commands.py
@@ -1,41 +1,34 @@
+# Copyright 2022 osfanbuff63
+#   
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from discord.ext import commands
-import discord
-from random import choice
 
-bot = commands.Bot(command_prefix="!", helpCommand=help)
+bot = commands.Bot(command_prefix="$?!", helpCommand=help)
 
-@bot.command(name="github")
-async def github(ctx):
-    await ctx.send("Check out osfanbuff63\"s projects on GitHub here! https://github.com/osfanbuff63")
+@bot.hybrid_command()
+async def code(ctx, help="Sends a link to osfanbuff63's GitHub page for all his projects."):
+    await ctx.send("Check out osfanbuff63's GitHub here! https://github.com/osfanbuff63")
 
-@bot.command(name="rules")
-async def rules(ctx):
-    await ctx.send(
-     "*This is copied from #info. Current as of August 7th, 2022*"
-    )
-# This command is disabled for now - i plan to split these up
-#@bot.group()
-#async def info(ctx, *, arg):
-#    if ctx.invoked_subcommand is None:
-#        await ctx.send("No subcommand provided, type `!help` for help!")
+@bot.hybrid_group(fallback="source")
+async def info(ctx, help="A bunch of utility commands related to the bot itself."):
+    await ctx.send("The source code for this bot can be found at https://github.com/osfanbuff63/discord-bot") # TODO: mirror to codeberg?
 
-#@info.command()
-#async def source(ctx):
-#    await ctx.send("Check out and contribute to the bot"s source code here: https://github.com/osfanbuff63/discord-bot")
+@info.hybrid_command()
+async def license(ctx, help="Shows license information for the bot."):
+    await ctx.send("osfanDiscordBot63 is licensed under the Apache License 2.0. You may obtain a copy of the License at https://www.apache.org/licenses/LICENSE-2.0. Run /info source for the source code.")
 
-#@info.command()
-#async def discord_bot(ctx):
-#    await ctx.send(
-#        "osfanbuff63\"s Discord Bot version {version}"
-#        "Licensed under the MIT License"
-#        "For the source code, run `!info source`"
-#    )
-
-@bot.command
-async def hug(ctx, arg):
-    await ctx.send(f"{ctx.message.author.mention} hugs {arg}")
-
-@bot.command
-async def user(ctx):
-    user = choice(ctx.message.channel.guild.members)
-    await ctx.send(f"Today\"s random user is {user}")
+@info.hybrid_command()
+async def ping(ctx, help="Pings the bot."):
+    latency_ms = round(bot.latency * 1000)
+    await ctx.send(f"Pong! Latency: {latency_ms}ms")
--- src/secrets.template.py
+++ src/secrets.template.py
@@ -12,11 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import discord
-import logging
-
-logger = logging.getLogger("discord")
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
-logger.addHandler(handler)
+# Credentials go here. The documentation has more info on this (or when it exists xD)
