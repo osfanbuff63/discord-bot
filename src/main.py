e80f74fed16ed406abbc930ded788ea9db494234
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2022-09-03 08:05:49 -0400

    Refactor to Docker and discord.py 2.0

--- src/main.py
+++ src/main.py
@@ -1,16 +1,34 @@
+# Copyright 2022 osfanbuff63
+#   
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import commands
 import logger
 import discord
 
-version = "v0.1.0-SNAPSHOT"
+global license
+global version
+global repo
+
+version = "v1.0.0"
 client = discord.Client()
 token = os.getenv("DISCORD_TOKEN")
 running_on = os.name
-license = "MIT License"
+license = "Apache License 2.0"
 repo = "https://github.com/osfanbuff63/discord-bot"
 
-print(f"osfanbuff63\"s Discord Bot version {version} running on {running_on}")
+print(f"osfanDiscordBot63 version {version} running on {running_on}")
 print(f"Licensed under the {license}")    
 print(f"Source code available here: {repo}")
 print("")
